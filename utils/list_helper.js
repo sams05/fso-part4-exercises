234d889be4aec1570423f1d7e4808fc156ac815f
Samuel Shen <sshen05@terpmail.umd.edu>
2024-02-26 11:58:50 -0500

    Finish ex4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
+/* eslint-disable-next-line no-unused-vars --
+ * blogs is not used, but should be kept to maintain the appropriate function signature
+**/
 const dummy = (blogs) => 1
+
+const totalLikes = (blogs) => blogs.reduce((sum, { likes }) => sum + likes, 0)
 
 module.exports = {
   dummy,
+  totalLikes,
 }
